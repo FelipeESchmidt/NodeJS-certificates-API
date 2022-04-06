be70adabd1cfd0f9131919bdbebc688d54e9acbf
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-06 14:29:03 -0300

    changing service name and removind unnecessary one

--- src/database/service/index.js
+++ src/database/service/index.js
@@ -3,5 +3,4 @@
  * de dados, não utilizam os models. Este papel pertence
  * aos services, também conhecidos como Repositórios.
  */
-export * from './orders.service';
-export * from './users.service';
+export * from './certificate.service';
