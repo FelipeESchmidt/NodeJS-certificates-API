eb95f405d42e67d22e340db8ca630bcd6e218f56
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-06 14:28:13 -0300

    changing database init to mongoose

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,27 +1,15 @@
 /* istanbul ignore file */
-/**
- * Este arquivo não é utilizado diretamente na
- * aplicação mas dá suporte aos models. Ele cria
- * uma instância do Sequelize utilizando o motor
- * para SQLite. Caso você queira utilizar outro
- * banco de dados, é aqui que você deverá fazer
- * a mudança.
- *
- * IMPORTANTE: não se esqueça de armazenar dados
- * sensíveis no arquivo .env e atualizar
- * @/utils/settings.js
- *
- * Mais informações: https://sequelize.org/master/
- */
-import { Sequelize } from 'sequelize';
-import { DATABASE_PATH } from '@/utils';
 
-export const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: DATABASE_PATH,
-  logging: false,
-});
+const mongoose = require('mongoose');
+import { DATABASE } from '@/utils';
 
-export const sync = async () => {
-  await sequelize.sync();
-};
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(DATABASE)
+  .then(() => {
+    console.log('Conectado ao MongoDB');
+  })
+  .catch(err => {
+    console.log(err);
+  });
